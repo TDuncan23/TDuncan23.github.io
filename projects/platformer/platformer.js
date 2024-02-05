82eaabbcd392b569038dcc3af044b8ce43993171
TDuncan23 <156357109+TDuncan23@users.noreply.github.com>
2024-02-05 18:49:16 +0000

    added plat and coll

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -59,9 +59,9 @@ $(function () {
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-    createCollectable("bron", 500, 699, 15, 0.45);  // creates a "steve" collectible at the coordinates (500, 699), falling with a high gravity of 15 and bouncing with 45% bounce
-    createCollectable("bosh", 800, 375); // creates a "steve" collectible at the coordinates (800, 375), falling with default gravity and bouncing with default bounce %
-    createCollectable("wade", 1300, 212, 48, 0.2); // creates a "grace" collectible at the coordinates (1300, 212), falling with a high gravity of 48 and bouncing with 20% bounce
+    createCollectable("steve", 500, 699, 15, 0.45);  // creates a "steve" collectible at the coordinates (500, 699), falling with a high gravity of 15 and bouncing with 45% bounce
+    createCollectable("steve", 800, 375); // creates a "steve" collectible at the coordinates (800, 375), falling with default gravity and bouncing with default bounce %
+    createCollectable("grace", 1300, 212, 48, 0.2); // creates a "grace" collectible at the coordinates (1300, 212), falling with a high gravity of 48 and bouncing with 20% bounce
 
     // TODO 3
     // Create cannons
