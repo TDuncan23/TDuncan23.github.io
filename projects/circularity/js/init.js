17ecf4da8c920f7fdbba13f1a4baf2f9b004718e
TDuncan23 <156357109+TDuncan23@users.noreply.github.com>
2024-04-19 17:55:36 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -28,11 +28,10 @@ var init = function (window) {
     }
 
     // TODO 3 / 7 : Call the drawCircle() function
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
+    var loopsCompleted = 0
+    for (var loopsCompleted = 0; loopsCompleted <= 100; loopsCompleted++) {
+      drawCircle();
+    }
     ////////////////////////////////////////////////////////////
     ///////////////// PROGRAM LOGIC ////////////////////////////
     ////////////////////////////////////////////////////////////
@@ -44,18 +43,14 @@ var init = function (window) {
         */
     function update() {
       // TODO 4 : Update the circle's position //
-     physikz.updatePosition(circles[0]);
-     physikz.updatePosition(circles[1]);
-     physikz.updatePosition(circles[2]);
-     physikz.updatePosition(circles[3]);
-     physikz.updatePosition(circles[4]);
+     
       // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-     game.checkCirclePosition(circles[0]);
-     game.checkCirclePosition(circles[1]);
-     game.checkCirclePosition(circles[2]);
-     game.checkCirclePosition(circles[3]);
-     game.checkCirclePosition(circles[4]);
+      
       // TODO 9 : Iterate over the array
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
     }
 
     /* 
@@ -70,7 +65,15 @@ var init = function (window) {
       }
 
       // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-
+      else if (circle.x < 0) {
+        circle.x = canvas.width;
+      }
+      else if (circle.y < 0) {
+        circle.y = canvas.height;
+      }
+      else if (circle.y > canvas.height) {
+        circle.y = 0;
+      }
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
     };
 
